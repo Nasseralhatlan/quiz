de2b05904672d363941f576309c6bacb8f5993df
Nasser Alhatlan <nasseralhatlan@gmail.com>
2020-04-23 02:21:07 +0300

    Added qustions total 10 qustions

---
+++ js/qustionsEN.js
@@ -0,0 +1,90 @@
+const quationsEN = [
+    {
+        txt : "Which player has the most UEFA Champions league final appearances",
+        img : "./img/qustions/Q1.jpg",
+        ans1 : ["Edwin van der Sar",0],
+        ans2 : ["Paolo maldini",1],
+        ans3 : ["Xavi",0],
+        ans4 : ["Iker casillas",0],
+    },
+    {
+        txt : "Which player is All-time UEFA Champions League final top  scorer",
+        img : "./img/qustions/Q2.jpg",
+        ans1 : ["Samuel Eto'o",0],
+        ans2 : ["Lionel messi",0],
+        ans3 : ["Cristiano Ronaldo",1],
+        ans4 : ["Hernán Crespo",0],
+    },
+    {
+        txt : "Which playe has the Most goals in a single UEFA Champions Leaguefinal",
+        img : "./img/qustions/Q3.jpg",
+        ans1 : ["Gareth Bale",1],
+        ans2 : ["David villa",0],
+        ans3 : ["Steven gerrard",0],
+        ans4 : ["Drogba",0],
+    },
+
+    {
+        txt : "Which coach has the most UEFA Champions League final wins",
+        img : "./img/qustions/Q4.jpg",
+        ans1 : ["Fabio capello",0],
+        ans2 : ["Louis van Gaal",0],
+        ans3 : ["Pep guardiola",0],
+        ans4 : ["Carlo Ancelotti",1],
+    },
+
+    {
+        txt : "Which coach has the most UEFA Champions league final appearances",
+        img : "./img/qustions/Q5.jpg",
+        ans1 : ["Diego simeone",0],
+        ans2 : ["Sir alex ferguson",1],
+        ans3 : ["Pep guardiola",0],
+        ans4 : ["Jurgen klopp",0],
+    },
+    {
+        txt : "Which player hasn't won Champions League with more than one club",
+        img : "./img/qustions/Q6.jpg",
+        ans1 : ["Samuel Eto'o",0],
+        ans2 : ["Clarence Seedorf",0],
+        ans3 : ["Cristiano Ronaldo",0],
+        ans4 : ["Luís Figo",1],
+    },
+    {
+        txt : "Out of these clubs which club has the most UEFA Champions league final appearances",
+        img : "./img/qustions/Q7.jpg",
+        ans1 : ["Ajax",0],
+        ans2 : ["Liverpool",0],
+        ans3 : ["Barcelona",1],
+        ans4 : ["Manchester United",0],
+    },
+    {
+        txt : "Which player has the most scoring ratio in UEFA champions League",
+        img : "./img/qustions/Q8.jpg",
+        ans1 : ["Lionel messi",1],
+        ans2 : ["robert lewandowski",0],
+        ans3 : ["Cristiano Ronaldo",0],
+        ans4 : ["Eusebio",0],
+    },
+    {
+        txt : "Which club has the Most successive UEFA Champions league final wins",
+        img : "./img/qustions/Q9.jpg",
+        ans1 : ["Ac milan",0],
+        ans2 : ["Real madrid",1],
+        ans3 : ["Barcelona",0],
+        ans4 : ["Juventus",0],
+    },
+    {
+        txt : "Which club has the most number of losses UEFA Champions League final",
+        img : "./img/qustions/Q10.jpg",
+        ans1 : ["Ac milan",0],
+        ans2 : ["Real madrid",1],
+        ans3 : ["Barcelona",0],
+        ans4 : ["Juventus",0],
+    },
+    
+
+    
+   
+    
+    
+]
